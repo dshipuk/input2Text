399c79a0082dba5be34c579f66dfa84e476de7c4
unknown <dshipuk@yahoo.com>
2022-04-28 13:12:16 -0700

    static now?

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-import InputForm from "./components/inputForm";
-
 function App() {
-  const [text, setText] = useState({
-    text: ""
-  })
 
-  const handleChanges = e => {
-    setText(e.target.value)
-  }
 
   return (
     <div className="App">
-      <h1>{text}</h1>
-      <label>Enter Text:
-        <input 
-          type="text"
-          name="text"
-          value={text}
-          onChange={handleChanges}
-        />
-      </label>
+      <h1>Welcome TO My Website</h1>
+      <p>David</p>
     </div>
   );
 }
